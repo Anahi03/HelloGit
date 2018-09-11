48d82ab2197980ffbadcb46e927ddbb69d7a32a7
Anahi03 <Anahi@Anahi-Laptop>
2018-09-10 21:47:41 -0500

    added a comment

--- GitHub/src/testHello/HelloGit.java
+++ GitHub/src/testHello/HelloGit.java
@@ -3,6 +3,8 @@ package testHello;
 public class HelloGit {
 
 	public static void main(String[] args) {
+		// a greeting
+		
 		System.out.print("\nHello Git");
 		// TODO Auto-generated method stub
 
